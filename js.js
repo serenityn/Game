2fe2248e689342a7a80b21953f46729b3dbffc3c
Serenity <serenitylowl@gmail.com>
2023-01-01 09:34:50 -0500

    Create one round option Test "rock" choice

---
+++ js.js
@@ -0,0 +1,20 @@
+
+function getComputerChoice() {
+    let arrGame = ["rock", "paper", "scissors"];
+    let randGame = arrGame[Math.floor(Math.random() * arrGame.length)];
+    return randGame;
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection ==="scissors") {
+        return "You Win! Rock beats Scissors.";
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You Lose! Paper beats Rock.";
+    } else {
+        return "You Tie!";
+    }
+    
+}
+const playerSselection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSselection, computerSelection));
