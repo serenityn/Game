19c984d7d3e62c844774e4bec39cd0cdd2b6ff0c
Serenity <serenitylowl@gmail.com>
2023-01-01 09:49:21 -0500

    Complete playRound function

--- js.js
+++ js.js
@@ -10,6 +10,14 @@ function playRound(playerSelection, computerSelection) {
         return "You Win! Rock beats Scissors.";
     } else if (playerSelection === "rock" && computerSelection === "paper") {
         return "You Lose! Paper beats Rock.";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You Win! Paper beats Rock.";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You Lose! Scissors beats Paper.";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You Win! Scissors beats Paper.";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You Lose! Rock beats Scissors.";
     } else {
         return "You Tie!";
     }
